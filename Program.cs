f7691c23afb74eead6d2108e725fc52ab2dee3ec
Lucy Laurentino <9998329@BCS.NET.NZ>
2017-08-31 16:50:09 +1200

    finish test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,66 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Start the program with Clear();
+            Console.Clear();
+            
+            /*Console.WriteLine("Enter in your first name");
+            var fname = Console.ReadLine();
+            Console.WriteLine("Enter in your last name");
+            var lname = Console.ReadLine();
+            Console.WriteLine($"Your name is {fname} {lname}");
+            Console.ReadLine();*/
+
+            
+            /*var counter = 12;
+
+            Console.WriteLine("Please enter in a number");
+            var number = int.Parse(Console.ReadLine());
+
+            for (var i = 1; i <= counter; i++)
+                {
+                Console.WriteLine($"{number} x {i} = {number * i}");
+                Console.ReadLine();
+
+                }*/
+
+
+            /*var counter = 12;
+            Console.WriteLine("Please enter in a number");
+            var number = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < counter; i++)
+                {
+                var a = i + 1;
+                Console.WriteLine($"{number} x {a} = {number * a}");
+                //Console.ReadLine();
+
+                }*/
+
+
+                /*var number = 0;
+                var counter = 5;
+
+                for (var i = 0; i < counter; i++)
+                {
+                    Console.WriteLine("Please enter in a number");
+                    var userInput = int.Parse(Console.ReadLine());
+
+                    number += userInput;
+                }
+                    Console.WriteLine($"The total is {number}");
+                    Console.WriteLine($"The average is {number/counter}");*/
+
+
+                    
+
+
+            //End the program with blank line and instructions
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+            
         }
     }
 }
